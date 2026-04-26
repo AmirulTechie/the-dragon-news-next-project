896eccb27869cd86890c775876c5e543fea9dfc0
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-26 19:45:04 +0600

    chore: install date-fns for dynamic dates

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,11 @@
-
+import logo from "@/assets/logo.png"
+import Image from "next/image";
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="text-center mx-auto py-8 space-y-4">
+            <Image src={logo} alt="Logo"></Image>
+            <p className="opacity-70">Journalism Without Fear or Favour</p>
+            <p>Date</p>
         </div>
     );
 };
