dbb5bf9b0f5420a81edda095917d7a8cf850e434
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-26 20:03:35 +0600

    feat: add not-found 404 page

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,12 @@
 import logo from "@/assets/logo.png"
 import Image from "next/image";
+import { format } from "date-fns";
 const Header = () => {
     return (
-        <div className="text-center mx-auto py-8 space-y-4">
+        <div className="text-center mx-auto py-8 space-y-3">
             <Image src={logo} alt="Logo"></Image>
             <p className="opacity-70">Journalism Without Fear or Favour</p>
-            <p>Date</p>
+            <p className="">{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
     );
 };
