225c3f58bbfea38b9d608f05d46c8bb3665f697a
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-04-28 21:27:54 +0600

    chore: add metadata to add website details

--- src/app/(main)/about/page.jsx
+++ src/app/(main)/about/page.jsx
@@ -1,8 +1,14 @@
+export const metadata = {
+    title: "The Dragon News - about us",
+    description: "Best news portal in Bangladesh",
+};
+
+
 const AboutPage = () => {
     return (
         <div>
             About Page
-        </div>
+        </div> 
     );
 };
 
